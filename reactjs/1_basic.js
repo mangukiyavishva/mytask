ba4b3235e55dc9313ccbed095910c6caa44e4394
Mangukiya Vishva <mangukiyavishva1@gmail.com>
2026-03-24 22:28:59 +0530

    Add files via upload

--- reactjs/1_basic.js
+++ reactjs/1_basic.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+
+//create a website with 3 pages home, about us and contact us using react router dom
+function Home(){
+  return (<div>
+    <h1>Home Page</h1>
+  </div>)
+}
+function Aboutus(){
+  return (<div>
+    <h1>About Us</h1>
+  </div>)
+}
+function Contactus(){
+  return (<div>
+    <h1>Contact Us</h1>
+  </div>)
+}
+
+function Website(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutus" element={<Aboutus />} />
+        <Route path="/contactus" element={<Contactus />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div>
-  <h1>Welcome to React!</h1>
-</div>);
+root.render(<Website />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
